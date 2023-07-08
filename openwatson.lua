ce30088539c1dad1f00daacb4f4282cac9f607e2
Kzx <138764294+kzx1337@users.noreply.github.com>
2023-07-08 10:41:44 +0200

    OpenWatson 1.0

--- openwatson.lua
+++ openwatson.lua
@@ -1,1 +1,12 @@
-
+return function (WatsonInfo)
+	local ver = "1.0"
+	print(WatsonInfo.CompanyName.. " is powered by OpenWatson "..ver.." (https://github.com/kzx1337/openwatson)")
+	local MarketPlaceService = game:GetService("MarketplaceService")
+	local GamepassCheck = MarketPlaceService:UserOwnsGamePassAsync(WatsonInfo.GameOwner, WatsonInfo.GamepassID)
+	if GamepassCheck then
+		print("[OpenWatson] License found for: "..WatsonInfo.ProductName.. " ("..WatsonInfo.GamepassID..")")
+		return true
+	end
+	print("[OpenWatson] You don't have a license for: "..WatsonInfo.ProductName.. " ("..WatsonInfo.GamepassID..")")
+	return false
+end
