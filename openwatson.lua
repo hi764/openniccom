d09547ffcd2bef64d809005bd79c2c630bd9c992
BlackHawk <85005142+hi764@users.noreply.github.com>
2023-07-11 06:54:03 -0500

    Update xylon.lua

--- openwatson.lua
+++ openwatson.lua
@@ -1,12 +1,12 @@
-return function (WatsonInfo)
+return function (XylonInfo)
 	local ver = "1.0"
-	print(WatsonInfo.CompanyName.. " is powered by OpenWatson "..ver.." (https://github.com/kzx1337/openwatson)")
+	print(XylonInfo.ProductName.. " is powered by Xylon, a whitelist owned by Niccom Solutions.")
 	local MarketPlaceService = game:GetService("MarketplaceService")
-	local GamepassCheck = MarketPlaceService:UserOwnsGamePassAsync(WatsonInfo.GameOwner, WatsonInfo.GamepassID)
+	local GamepassCheck = MarketPlaceService:UserOwnsGamePassAsync(XylonInfo.GameOwner, XylonInfo.GamepassID)
 	if GamepassCheck then
-		print("[OpenWatson] License found for: "..WatsonInfo.ProductName.. " ("..WatsonInfo.GamepassID..")")
+		print("[Xylon] License found for: "..XylonInfo.ProductName.. " ("..XylonInfo.GamepassID..")")
 		return true
 	end
-	print("[OpenWatson] You don't have a license for: "..WatsonInfo.ProductName.. " ("..WatsonInfo.GamepassID..")")
+	print("[Xylon] You don't have a license for: "..XylonInfo.ProductName.. " ("..XylonInfo.GamepassID..")")
 	return false
 end
